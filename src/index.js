3a298b260e21e708b803f6fbdac8e8f663000138
Crenby <arkadiy.shtolts@mail.ru>
2024-05-16 15:00:41 +0300

    Step 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'node:process';
+import diff from './diff.js'
 
 function parser (path1, path2) {
   const fullPath1 = path.resolve(process.cwd(), path1); 
@@ -9,7 +10,7 @@ function parser (path1, path2) {
   const fullPath2 = path.resolve(process.cwd(), path2); 
   const data2 = JSON.parse(fs.readFileSync(fullPath2));
 
-  return [data1, data2];
+  return diff(data1, data2);
 };
 
 export default parser;
