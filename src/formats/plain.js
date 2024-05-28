0bcd8c55ca185ca53dffb0986e904f05dd331752
Crenby <arkadiy.shtolts@mail.ru>
2024-05-28 16:13:50 +0300

    plain v1.1

--- src/formats/plain.js
+++ src/formats/plain.js
@@ -2,15 +2,16 @@ import { isObject } from "./stylish.js";
 
 function genPlain(data, name = "") {
   let str = "";
+  const complexValue = '[complex value]';
   data.map((item) => {
     if(item.type === 'deleted') {
       str += `Property '${name}${item.key}' was removed\n`;
     }
     if(item.type === 'added') { 
-      str += `Property '${name}${item.key}' was added with value: ${isObject(item.value) ? '[complex value]' : item.value}\n`;
+      str += `Property '${name}${item.key}' was added with value: ${isObject(item.value) ? complexValue : `'${item.value}'`}\n`;
     }
     if(item.type === 'changed') { 
-      str += `Property '${name}${item.key}' was updated. From '${isObject(item.value1) ? '[complex value]' : item.value1}' to '${item.value2}'\n`;
+      str += `Property '${name}${item.key}' was updated. From ${isObject(item.value1) ? complexValue : `'${item.value1}'`} to ${isObject(item.value1) ? complexValue : `'${item.value2}'`}\n`;
     }
     if(item.type === 'nested') {
       str += genPlain(item.children, name + `${item.key}.`);
