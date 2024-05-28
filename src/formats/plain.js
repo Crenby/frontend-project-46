3437a3ff67942694651ea834c665b388b1de5045
Crenby <arkadiy.shtolts@mail.ru>
2024-05-28 16:26:57 +0300

    plain v1.2

--- src/formats/plain.js
+++ src/formats/plain.js
@@ -8,10 +8,11 @@ function genPlain(data, name = "") {
       str += `Property '${name}${item.key}' was removed\n`;
     }
     if(item.type === 'added') { 
-      str += `Property '${name}${item.key}' was added with value: ${isObject(item.value) ? complexValue : `'${item.value}'`}\n`;
+      //str += `Property '${name}${item.key}' was added with value: ${isObject(item.value) ? complexValue : `'${item.value}'`}\n`;
+      str += `Property '${name}${item.key}' was added with value: ${isObject(item.value) ? complexValue : typeof(item.value) === 'string' ? `'${item.value}'` : item.value}\n`;
     }
     if(item.type === 'changed') { 
-      str += `Property '${name}${item.key}' was updated. From ${isObject(item.value1) ? complexValue : `'${item.value1}'`} to ${isObject(item.value1) ? complexValue : `'${item.value2}'`}\n`;
+      str += `Property '${name}${item.key}' was updated. From ${isObject(item.value1) ? complexValue : typeof(item.value1) === 'string' ? `'${item.value1}'` : item.value1} to ${isObject(item.value2) ? complexValue : typeof(item.value2) === 'string' ? `'${item.value2}'` : item.value2}\n`;
     }
     if(item.type === 'nested') {
       str += genPlain(item.children, name + `${item.key}.`);
