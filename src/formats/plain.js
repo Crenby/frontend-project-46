474fd8c5580fdf3ec5fa3d663f2bf7bbf7d8ab51
Crenby <arkadiy.shtolts@mail.ru>
2024-05-29 10:48:37 +0300

    eslint repair v1.3

--- src/formats/plain.js
+++ src/formats/plain.js
@@ -1,25 +1,33 @@
 import { isObject } from './stylish.js';
 
+function prepareValue(value) {
+  if (isObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return value;
+}
+
 function genPlain(data, name = '') {
-  let str = '';
-  const complexValue = '[complex value]';
+  const str = [];
   data.map((item) => {
     if (item.type === 'deleted') {
-      str += `Property '${name}${item.key}' was removed\n`;
+      str.push(`Property '${name}${item.key}' was removed\n`);
     }
     if (item.type === 'added') {
-      str += `Property '${name}${item.key}' was added with value: ${isObject(item.value) ? complexValue : typeof (item.value) === 'string' ? `'${item.value}'` : item.value}\n`;
+      str.push(`Property '${name}${item.key}' was added with value: ${prepareValue(item.value)}\n`);
     }
     if (item.type === 'changed') {
-      str += `Property '${name}${item.key}' was updated. From ${isObject(item.value1) ? complexValue : typeof (item.value1) === 'string' ? `'${item.value1}'` : item.value1} to ${isObject(item.value2) ? complexValue : typeof (item.value2) === 'string' ? `'${item.value2}'` : item.value2}\n`;
+      str.push(`Property '${name}${item.key}' was updated. From ${prepareValue(item.value1)} to ${prepareValue(item.value2)}\n`);
     }
     if (item.type === 'nested') {
-      str += genPlain(item.children, `${name}${item.key}.`);
+      str.push(genPlain(item.children, `${name}${item.key}.`));
     }
     return true;
   });
-
-  return str;
+  return str.join('');
 }
 
 function genPlainWithoutSpaces(data) {
